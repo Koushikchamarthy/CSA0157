035744a091473f98e63757473293404d02c13cd4
Koushikchamarthy <122337562+Koushikchamarthy@users.noreply.github.com>
2023-01-11 13:50:58 +0530

    Add files via upload

---
+++ Exp 29.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void swap(int *x, int *y) {
+    *x = *x + *y;
+    *y = *x - *y;
+    *x = *x - *y;
+}
+
+int main() {
+    int x = 5, y = 10;
+    printf("Before swapping: x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("After swapping: x = %d, y = %d\n", x, y);
+    return 0;
+}
+
